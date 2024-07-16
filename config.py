05bd37be796b49747fca07629adbd0f9c43cce2a
RohitDdos1 <rohit002676@gmail.com>
2024-07-16 16:37:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7249311521:AAHFQTTzXmzEuNyzIJYzloC4EfQ_P3WlvCE")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@Rohitkumar_9_9_9")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6591143473")
 
 
 
